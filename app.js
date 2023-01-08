cd0667605a846369317be0d25f58b81c36d59e66
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-08 15:16:55 +0100

    init: add main files

---
+++ app.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+})();
